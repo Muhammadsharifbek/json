45d06876f93638e04023321a62dce7c99f3289b4
Muhammadsharifbek <sharifahmadov690@gmail.com>
2024-04-29 14:54:56 +0500

    add new import ErrorPage and new hook

--- src/components/BreadCrumbs.jsx
+++ src/components/BreadCrumbs.jsx
@@ -20,3 +20,19 @@ function BreadCrumbs() {
   return <div className="bread-crumbs">{crumbs}</div>;
 }
 export default BreadCrumbs;
+
+// const location = useLocation();
+
+// const currentLink = [];
+
+// const crumbs = location.pathname
+//   .split("/")
+//   .filter((crumb) => crumb != "")
+//   .map((crumb) => {
+//     currentLink.push(`/${crumb}`);
+//     return (
+//       <div className="crumb" key={crumb}>
+//         <NavLink to={currentLink.join("")}>{crumb}</NavLink>
+//       </div>
+//     );
+//   });
